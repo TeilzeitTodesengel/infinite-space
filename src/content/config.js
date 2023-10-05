22227fc8f5d1d33dc7eac6f71cdeb3850cd05613
Teilzeit Todesengel <teilzeit.todesengel2605@gmail.com>
2023-10-05 12:05:22 +0200

    added new wiki part

--- src/content/config.js
+++ src/content/config.js
@@ -1,23 +1,31 @@
 import { defineCollection, z } from 'astro:content';
 
-const factionsCollection = defineCollection({
+const companiesCollection = defineCollection({
     schema: z.object ({
         title: z.string(),
-        tags: z.array(z.string()),
+        tags: z.array(z.string()).optional(),
         image: z.string().optional(),
     }),
 });
-
+const factionsCollection = defineCollection({
+    schema: z.object ({
+        title: z.string(),
+        tags: z.array(z.string()).optional(),
+        image: z.string().optional(),
+        icon: z.string().optional(),
+    }),
+});
 const personasCollection = defineCollection({
     schema: z.object ({
         name: z.string(),
-        tags: z.array(z.string()),
+        tags: z.array(z.string()).optional(),
         image: z.string().optional(),
         icon: z.string().optional(),
     }),
 });
 
 export const collections = {
+    'companies': companiesCollection,
     'factions': factionsCollection,
     'personas': personasCollection,
 };
