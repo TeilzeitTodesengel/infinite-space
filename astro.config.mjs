8abc548a7905a7beb1fc7b3e7aceae2fdeec518d
TeilzeitTodesengel <teilzeit.todesengel2605@gmail.com>
2023-10-05 21:46:13 +0200

    fix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site: 'https://astronaut.github.io',
+  site: 'teilzeittodesengel.github.io',
 });
